f828259b7a672a3672bf2cf4654b19e8b2657c10
Kei <kei.n140802@gmail.com>
2022-07-05 23:00:13 +0900

    タイムゾーンを日本に設定

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,9 @@ module ManyoExam
         # デフォルトの言語設定
         # config.i18n.default_locale = :en
         config.i18n.default_locale = :ja
+
+        config.time_zone = 'Tokyo'
+        config.active_record.default_timezone = :local
     end
 
     # Settings in config/environments/* take precedence over those specified here.
