961159f48b585d68c85dcd6dcd97f6e226bcced2
Kei <kei.n140802@gmail.com>
2022-07-14 18:50:16 +0900

    scaffoldでuserを作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_13_104115) do
+ActiveRecord::Schema.define(version: 2022_07_14_092319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 2022_07_13_104115) do
     t.integer "status", default: 1, null: false
     t.integer "priority", default: 0, null: false
     t.index ["title"], name: "index_tasks_on_title"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
